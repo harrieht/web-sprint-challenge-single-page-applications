809f9bee99a41f55a9aca99a5d850601d8d9ee23
Mary Gabon <marygabon21@gmail.com>
2023-05-16 10:16:22 -0700

    update MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link  } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <h1>Welcome to Pizza App</h1>
+      <Link to ="/pizza" id="order-pizza">
+        Order Pizza
+      </Link>
+    </div>
   );
 };
 export default App;
